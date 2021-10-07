d5cd9318eff23a08da16dd6c6988d355ecc64d36
shixy <shixiayupan@qq.com>
2021-10-07 21:16:36 +0800

    2 commit

--- src/main/java/com/nbtt/App.java
+++ src/main/java/com/nbtt/App.java
@@ -5,6 +5,6 @@ package com.nbtt;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World 1");
     }
 }
