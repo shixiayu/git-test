5988d8aa1bfd1f3d3857cbda7cd0ac2d85a4cb4a
shixy <shixiayupan@qq.com>
2021-10-07 21:32:48 +0800

    hot-fix 3 commit

--- src/main/java/com/nbtt/App.java
+++ src/main/java/com/nbtt/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World 2");
         System.out.println("Hello World hot-fix");
+        System.out.println("Hello World hot-fix1");
     }
 }
